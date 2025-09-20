b6330af52ce75b8460f5e083fbfb93f2e935c755
psamirt <p_samir@hotmail.com>
2025-09-20 15:05:41 -0500

    estilos

--- src/components/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp.tsx
@@ -9,7 +9,7 @@ const FloatingWhatsApp = () => {
       className="fixed bottom-6 right-6 z-50 border-2 border-white rounded-full"
     >
       <FaWhatsapp
-        className="bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg w-16 h-16 p-3"
+        className="bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg w-12 h-12 md:w-16 md:h-16 p-3"
         size={20}
       />
     </a>
